d372085c1f6240fee9c07247b6b68ff85a7f945c
Henrique S <henriques84282020@gmail.com>
2026-06-29 11:41:00 -0300

    chore: initial project setup

---
+++ api_gestaocoworking/src/main/java/com/coworking/api_gestaocoworking/ApiGestaocoworkingApplication.java
@@ -0,0 +1,13 @@
+package com.coworking.api_gestaocoworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestaocoworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestaocoworkingApplication.class, args);
+	}
+
+}
---
+++ api_gestaocoworking/src/test/java/com/coworking/api_gestaocoworking/ApiGestaocoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coworking.api_gestaocoworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestaocoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
